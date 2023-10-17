b97586900e1c415024c2efffdf8a78a6f86575f3
조범준 <141565207+KimMaYa1@users.noreply.github.com>
2023-10-17 11:29:06 +0900

    데미지 받았을때 화면 빨갛게

---
+++ Chapter3-3_B07_Project/Assets/Scripts/UI/DamageIndicator.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DamageIndicator : MonoBehaviour
+{
+    [SerializeField] private Image damgeImage;
+    [SerializeField] private float flashSpeed = 1f;
+
+    private Coroutine coroutine;
+
+    public void Flash()
+    {
+        if (coroutine != null)
+        {
+            StopCoroutine(coroutine);
+        }
+
+        damgeImage.gameObject.SetActive(true);
+        coroutine = StartCoroutine(FadeAway());
+    }
+
+    private IEnumerator FadeAway()
+    {
+        float startAlpha = 1f;
+        float a = startAlpha;
+
+        while (a > 0.0f)
+        {
+            a -= (startAlpha / flashSpeed) * Time.deltaTime;
+            damgeImage.color = new Color(1.0f, 0.0f, 0.0f, a);
+            yield return null;
+        }
+
+        damgeImage.gameObject.SetActive(false);
+        yield break;
+    }
+}
