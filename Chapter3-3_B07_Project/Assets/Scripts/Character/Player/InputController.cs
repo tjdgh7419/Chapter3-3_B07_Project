61f71a6fa3608af362d1cb2a511d027e52cfb92e
tjdgh7419 <70570791+tjdgh7419@users.noreply.github.com>
2023-10-18 15:19:22 +0900

    [UPDATE] 플레이어 움직임, Player.cs, InputController.cs, PlayerMovement.cs 추가

---
+++ Chapter3-3_B07_Project/Assets/Scripts/Character/Player/InputController.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputController : MonoBehaviour
+{
+	public event Action<Vector2> OnMoved;
+	public event Action<Vector2> OnLookRotation;
+	public event Action OnJump;
+
+	private void Awake()
+	{
+		if (TryGetComponent(out PlayerInput playerInput))
+		{
+			Debug.Log("11");
+			InputAction action = playerInput.actions.FindAction("Move");
+			action.performed += CallOnMove;
+			action.canceled += CallOnMove;
+			action = playerInput.actions.FindAction("Look");
+			action.performed += CallOnLookRotation;
+			action.canceled += CallOnLookRotation;
+			//action = playerInput.actions.FindAction("Jump");
+			//action.started += CallOnJump;
+		}
+	}
+
+	public void CallOnMove(InputAction.CallbackContext callbackContext)
+	{
+		Vector2 direction = callbackContext.ReadValue<Vector2>();
+		direction.Normalize();
+		OnMoved?.Invoke(direction);
+	}
+
+	public void CallOnLookRotation(InputAction.CallbackContext callbackContext)
+	{
+		Vector2 mouseDelta = callbackContext.ReadValue<Vector2>();
+		OnLookRotation?.Invoke(mouseDelta);
+	}
+
+	//public void CallOnJump(InputAction.CallbackContext callbackContext)
+	//{
+	//	OnJump?.Invoke();
+	//}
+}
